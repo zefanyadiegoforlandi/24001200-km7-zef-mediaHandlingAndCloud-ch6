b609f20e866d20b4a5808eb5a573628e3e119808
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-11-07 12:37:53 +0700

    fitur update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const path = require('path'); 
 const router = require('express').Router();
+const MediaHandling = require('../controllers/mediaHandling');
+const multer = require('../libs/multer');
 
+router.put('/update-image/:id', multer.single('image'), MediaHandling.updateImage);
 
 module.exports = router;
