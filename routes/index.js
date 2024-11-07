3423c9ac02b42829c7588afcbac8507009aa584e
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-11-07 12:43:54 +0700

    dev commit4

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 const path = require('path'); 
 const router = require('express').Router();
-const MediaHandling = require('../controllers/mediaHandling');
-const multer = require('../libs/multer');
-
-router.put('/update-image/:id', multer.single('image'), MediaHandling.updateImage);
+//ini adalah dev
 
 module.exports = router;
