ecd3521e108f2ce36e916498921d10bbc0b551d8
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-11-07 12:34:22 +0700

    fitur upload

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const path = require('path'); 
 const router = require('express').Router();
+const MediaHandling = require('../controllers/mediaHandling');
+const multer = require('../libs/multer');
+
+router.post('/upload-image', multer.single('image'), MediaHandling.uploadImage);
 
 
 module.exports = router;
