ca2b8446f12b9ae06486c97208a4fb95ce385a8e
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-11-10 14:24:45 +0700

    aws

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const routes = require('./routes');
 
 app.use(express.json());
